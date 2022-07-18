c68d538cb7c18657c4bf1b09476a4e62c2e38c8a
Lar Phaw Htoo <larphawhtoo6@gmail.com>
2022-07-18 12:29:15 +0630

    commit web module 18/7/2022 12:28

--- Node-Tutotial/app.js
+++ Node-Tutotial/app.js
@@ -1,99 +1,37 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-const { Console } = require('console');
-var fs = require('fs');
+// listener #1
+var listner1 = function listner1() {
+   console.log('listner1 executed.');
+}
 
-//Blocking Code
-var data = fs.readFileSync('input.txt');
-console.log(data.toString());
-console.log("Program Ended");
+// listener #2
+var listner2 = function listner2() {
+   console.log('listner2 executed.');
+}
 
-//Non blocking Code
-fs.readFile('input.txt', function (err, data) {
-    if (err)
-        return console.error(err);
-        console.log(data.toString());
-});
-console.log("Program Ended");
+// Bind the connection event with the listner1 function
+eventEmitter.addListener('connection', listner1);
 
-//File Open
-console.log("Going to open file!");
-fs.open('input.txt', 'r+', function(err, fd) {
-   if (err) {
-      return console.error(err);
-   }
-   console.log("File opened successfully!");     
-});
+// Bind the connection event with the listner2 function
+eventEmitter.on('connection', listner2);
 
+var eventListeners = require('events').EventEmitter.listenerCount
+   (eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
 
-//Writable
-var data = 'Simply Easy Learning';
+// Fire the connection event 
+eventEmitter.emit('connection');
 
-// Create a writable stream
-var writerStream = fs.createWriteStream('output.txt');
+// Remove the binding of listner1 function
+eventEmitter.removeListener('connection', listner1);
+console.log("Listner1 will not listen now.");
 
-// Write the data to stream with encoding to be utf8
-writerStream.write(data,'UTF8');
+// Fire the connection event 
+eventEmitter.emit('connection');
 
-// Mark the end of file
-writerStream.end();
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
 
-// Handle stream events --> finish, and error
-writerStream.on('finish', function() {
-   console.log("Write completed.");
-});
-
-writerStream.on('error', function(err) {
-   console.log(err.stack);
-});
-
-console.log("Program Ended");
-
-//Piping
-
-var readerStream = fs.createReadStream('input.txt');
-
-// Create a writable stream
-var writerStream = fs.createWriteStream('output.txt');
-
-// Pipe the read and write operations
-// read input.txt and write data to output.txt
-readerStream.pipe(writerStream);
-
-console.log("Program Ended");
-
-//Chainging the Stream
-var zlib = require('zlib');
-
-// Compress the file input.txt to input.txt.gz
-//fs.createReadStream('input.txt')
-//   .pipe(zlib.createGzip())
-//   .pipe(fs.createWriteStream('input.txt.gz'));
-//  
-//console.log("File Compressed.");
-
-fs.createReadStream('input.txt.gz')
-   .pipe(zlib.createGunzip())
-   .pipe(fs.createWriteStream('input.txt'));
-  
-console.log("File Decompressed.");
-
-//Create Directory
-console.log("Going to create directory /tmp/test");
-fs.mkdir('test',function(err) {
-   if (err) {
-      return console.error(err);
-   }
-   console.log("Directory created successfully!");
-});
-
-//Read Directory
-
-console.log("Going to read directory");
-fs.readdir("/",function(err, files) {
-   if (err) {
-      return console.error(err);
-   }
-   files.forEach( function (file) {
-      console.log( file );
-   });
-});
+console.log("Program Ended.");
