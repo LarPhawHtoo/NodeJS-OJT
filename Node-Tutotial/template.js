4eabca3602f9b880c1a62c71baf70b1e0ba3ec5f
Lar Phaw Htoo <larphawhtoo6@gmail.com>
2022-07-22 20:21:43 +0630

    form data 22/7/2022 commit

--- Node-Tutotial/template.js
+++ Node-Tutotial/template.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'pug');
-app.set('views','views');
+app.set('views', 'views');
 
 app.get('/dynamic_view', function(req, res){
    res.render('dynamic', {
