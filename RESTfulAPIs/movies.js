c2d63816c6f2282c48696fbae7897c2b38abec6e
Lar Phaw Htoo <larphawhtoo6@gmail.com>
2022-07-27 16:15:26 +0630

    RESTful APIs 27/7/2022 commit

---
+++ RESTfulAPIs/movies.js
@@ -0,0 +1,103 @@
+var express = require('express');
+
+var router = express.Router();
+
+var movies = [
+    { id: 101, name: "Fight Club", year: 1999, rating: 8.1 },
+    { id: 102, name: "Inception", year: 2010, rating: 8.7 },
+    { id: 103, name: "The Dark Knight", year: 2008, rating: 9 },
+    { id: 104, name: "12 Angry Men", year: 1957, rating: 8.9 }
+];
+
+//Routes will go here
+router.get('/', (req, res) => {
+    console.log(movies);
+    res.json(movies);
+})
+
+router.get('/:id([0-9]{3,})', (req, res) => {
+    var currMovie = movies.filter(function (movie) {
+        if (movie.id == req.params.id) {
+            return true;
+        }
+    });
+    if (currMovie.length == 1) {
+        res.json(currMovie[0])
+    } else {
+        res.status(404); //Set status to 404 as movie was not found
+        res.json({ message: "Not Found" });
+    }
+});
+
+router.post('/', (req, res) => {
+    //Check if all fields are provided and are valid
+    if (!req.body.name ||
+        !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+        !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)) {
+        
+        res.status(400);
+        res.json({ message: "Bad Request" });
+    }
+    else {
+        var newId = movies[movies.length - 1].id + 1;
+        movies.push({
+            id: newId,
+            name: req.body.name,
+            year: req.body.year,
+            rating: req.body.rating
+        });
+        res.json({ message: "New movie created. ", location: "/movies/" + newId });
+    }
+        
+});
+
+router.put('/:id', (req, res) => {
+    if (!req.body.name ||
+        !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+        !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+        !req.params.id.toString().match(/^[0-9]{3,}$/g)) {
+        res.status(400);
+        res.json({ message: "Bad Request" });
+    } else {
+        //Get us the index of movie with given id
+        var updateIndex = movies.map(function (movie) {
+            return movie.id;
+        }).indexOf(parseInt(req.params.id));
+
+        if (updateIndex === -1) {
+            //movie not found , create new
+            movies.push({
+                id: req.params.id,
+                name: req.body.name,
+                year: req.body.year,
+                rating: req.body.rating
+            });
+            res.json({ message: "New movie created.", location: "/movies/" + req.params.id });
+        } else {
+            //Update existing movie
+            movies[updateIndex] = {
+                id: req.params.id,
+                name: req.body.name,
+                year: req.body.year,
+                rating: req.body.rating
+            };
+            console.log(movies);
+            res.json({ message: "Movie id " + req.params.id + " updated.", location: "/movies/" + req.params.id });
+        }
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    var removeIndex = movies.map(function (movie) {
+        return movie.id;
+    }).indexOf(req.params.id); //Get us the index of movie with given id
+
+    if (removeIndex === 1) {
+        res.json({ message: "Not Found" });
+    } else {
+        movies.splice(removeIndex, 1);
+        res.send({ message: "Remove id " + req.params.id + " removed." });
+    }
+});
+
+module.exports = router;
